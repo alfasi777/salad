b14fd5090f0e76b9e582817f09c5495c607bbc26
alfasi777 <asafb.amal@gmail.com>
2025-03-17 11:20:44 +0200

    WhatsApp

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -36,7 +36,7 @@ const Footer = () => {
             </a>
             <a
             className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-            href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            href="https://wa.me/972528688999"
             target="_blank"
             rel="noopener noreferrer"
             >
